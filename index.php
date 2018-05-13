9e5e432c06f042e355d095b497be279968eca75c
dixanta <dixanta@gmail.com>
2018-05-13 15:42:38 +0545

    bootstrap

--- index.php
+++ index.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>App</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css"/>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
 </head>
 <body>
+    <div class="container">
     <?php for($i=1;$i<=10;$i++):?>
     <h1><?php echo "Hello world";?></h1>
-<?php endfor;?>
+    <?php endfor;?>
+</div>
 </body>
 </html>
